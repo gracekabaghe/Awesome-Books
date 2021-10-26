baeef72af3363bbc18ad73946915d2a60d78a63a
gracekabaghe <gracema2002@gmail.com>
2021-10-26 10:39:02 +0200

    Added local storage

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const awesomeBooks = [
-    {
-        title: 'Book one',
-        author: 'John Gray',
-        buttonRemove: 'Remove',
-        
-    },
-    {
-        title: 'Book two',
-        author: 'Maggie Gray',
-        buttonRemove: 'Remove',
-       
-    },
-    {
-        title: 'Book three',
-        author: 'Bill Gates',
-        buttonRemove: 'Remove',
-        
-    },
-];
-for(i = 0; i < awesomeBooks.length; i++) {
-    const details = awesomeBooks[i];
-    ourZone.innerHTML += `
-    <P>${details.title}</P>
-    <P>${details.author}</P>
-    <button type="button">${details.buttonRemove}</button>
-    <hr>
-    `;
+const bookList = document.querySelector('.books');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+
+const Book = function theBook(title, author) {
+  this.title = title;
+  this.author = author;
+};
+
+let storedData = [];
+
+function addBookToList(newBook) {
+  const bookTemp = `
+    <div class="book">
+      <h2>Title: ${newBook.title}</h2>
+      <h2>Author: ${newBook.author}</h2>
+      <button class="remove" type="button">Remove</button>
+      <hr>
+    </div>
+  `;
+  document.querySelector('.books').innerHTML += bookTemp;
+  return document.querySelector('.books').innerHTML;
 }
 
-
-  
+document.addEventListener('DOMContentLoaded', () => {
+  if (bookList !== null) {
+    storedData = [...JSON.parse(localStorage.getItem('localBookList'))];
+    storedData.forEach((item) => {
+      addBookToList(item);
+    });
+  }
+});
